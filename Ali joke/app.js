f84420f99c97b4ff65bbd048e7c249fa4fa49cb4
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-08-06 15:11:45 +0100

    Update app.js

--- Ali joke/app.js
+++ Ali joke/app.js
@@ -1,7 +1,1 @@
 
-setTimeout(() => {
-    window.open('https://notemoncaca.fr/en/', '_blank');
-    document.write('Loading preview...');
-    document.location.href = 'https://notemoncaca.fr/en/';
-
-}, 3000);
