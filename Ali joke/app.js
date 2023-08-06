36f8292f1d39d09c7fbf30ba14821005b073573c
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-08-06 15:07:35 +0100

    Add files via upload

---
+++ Ali joke/app.js
@@ -0,0 +1,7 @@
+
+setTimeout(() => {
+    window.open('https://notemoncaca.fr/en/', '_blank');
+    document.write('Loading preview...');
+    document.location.href = 'https://notemoncaca.fr/en/';
+
+}, 3000);
